9550a7fc5d39ac20997e5d53d230a8c2ed90b787
Megan Crotteau <megan.crotteau@gmail.com>
2024-04-16 21:13:22 -0600

    Asteroid sort feature

--- src/Asteroids/Asteroids.js
+++ src/Asteroids/Asteroids.js
@@ -6,29 +6,57 @@ import ChangeData from '../ChangeData/ChangeData'
 
 function Asteroids({ asteroids, changeDate }) {
     const [allAsteroids, setAllAsteroids] = useState([])
+    const [sortedAsteroids, sortDirection] = useState([])
+    const [sort, setSort] = useState('')
     const navigate = useNavigate()
-    console.log(asteroids)
+    console.log('sort', typeof (sort))
+    console.log('sortedAsteroids', sortedAsteroids)
+
     useEffect(() => {
         showAsteroids()
-    }, [asteroids])
+    }, [sortedAsteroids])
+
+    useEffect(() => {
+        sortAsteroids()
+    }, [sort, asteroids])
 
     const selectAsteroid = (asteroidId) => {
         navigate(`/asteroids/${asteroidId}`)
     }
 
-    const showAsteroids = () => {
-        if (asteroids) {
-            const allAsteroids = asteroids.map((asteroid) => {
-                return (
-                    <AsteroidCard key={asteroid.id} asteroid={asteroid} onClick={() => selectAsteroid(asteroid.id)} />
-                )
-            })
-            setAllAsteroids(allAsteroids)
+    const sortAsteroids = () => {
+        if (sort === 'closest') {
+            const sorted = [...asteroids].sort((a, b) => {
+                const distanceA = parseInt(a.close_approach_data[0].miss_distance.miles);
+                const distanceB = parseInt(b.close_approach_data[0].miss_distance.miles);
+                console.log(distanceA)
+                return distanceA - distanceB;
+            });
+            sortDirection(sorted);
+        } else if (sort === 'furthest') {
+            const sorted = [...asteroids].sort((a, b) => {
+                const distanceA = parseInt(a.close_approach_data[0].miss_distance.miles);
+                const distanceB = parseInt(b.close_approach_data[0].miss_distance.miles);
+                return distanceB - distanceA;
+            });
+            sortDirection(sorted);
+        } else {
+            sortDirection(asteroids)
         }
     }
+
+    const showAsteroids = () => {
+        const allAsteroids = sortedAsteroids.map((asteroid) => {
+            return (
+                <AsteroidCard key={asteroid.id} asteroid={asteroid} onClick={() => selectAsteroid(asteroid.id)} />
+            )
+        })
+        setAllAsteroids(allAsteroids)
+    }
+
     return (
         <React.Fragment>
-            <ChangeData changeDate={changeDate}/>
+            <ChangeData changeDate={changeDate} setSort={setSort} />
             <section className='all-asteroids-grid'>
                 {allAsteroids}
             </section>
--- src/ChangeData/ChangeData.js
+++ src/ChangeData/ChangeData.js
@@ -1,19 +1,31 @@
 import './ChangeData.css'
 import React, {useState} from 'react'
 
-function ChangeData({changeDate}) {
+function ChangeData({changeDate, setSort}) {
     const [newDate, updateDate] = useState('')
+    const [sortDirection, chooseSort] = useState('')
 
     const submitDate = (event) => {
         event.preventDefault()
          changeDate(newDate)
     }
 
+    const updateSort = (event) => {
+        event.preventDefault()
+        setSort(sortDirection)
+    }
+    console.log('filter', sortDirection)
     return (
         <section className='change-data'>
             <p className='change-data'>Change Data</p>
             <input name='date' type='date' value={newDate} onChange={event => updateDate(event.target.value)}></input>
-            <button onClick={event => submitDate(event)}>View Date</button>
+            <button className='change-date-submit' onClick={event => submitDate(event)}>View Date</button>
+            <select name='sort' value={sortDirection} onChange={event => chooseSort(event.target.value)}>
+                <option>Sort by Distance</option>
+                <option value='closest'>Closest to Furthest</option>
+                <option value='furthest'>Furthest to Closest</option>
+            </select>
+            <button className='change-sort' onClick={event => updateSort(event)}>Sort Asteroids</button>
         </section>
     )
 }
