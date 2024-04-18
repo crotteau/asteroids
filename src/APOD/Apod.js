5b645b426a8397485d76f6efee8de5d9713aabe2
Megan Crotteau <megan.crotteau@gmail.com>
2024-04-18 17:05:26 -0600

    Basic apod styling complete

--- src/APOD/Apod.js
+++ src/APOD/Apod.js
@@ -1,19 +1,21 @@
 import './Apod.css'
 import { NavLink } from 'react-router-dom';
+import asteroidImage from '../assets/asteroid.png'
+import React from 'react';
 
 function Apod({ photo }) {
 
     return (
-        <section className='apod'>
-            <article className='daily-photo-info'>
+        <section className='apod' style={{ backgroundImage: `url(${asteroidImage})` }}>
+            <article className='apod-photo-info'>
                 <h2>Astronomy Picture of the Day</h2>
-                <h3 className='daily-photo-title'>{photo.title}</h3>
-                <p className='daily-photo-cite'>{photo.copyright}</p>
-                <p className='daily-photo-explanation'>{photo.explanation}</p>
-                <a href={photo.url}>View Fullsize Image</a>
-                <NavLink to='/' className='view-asteroids'>Back to Main</NavLink>
+                <h3 className='apod-photo-title'>{photo.title}</h3>
+                <p className='apod-photo-cite'>{photo.copyright}</p>
+                <p className='apod-photo-explanation'>{photo.explanation}</p>
+                <a className='view-fullsize-apod' href={photo.url}>View Fullsize Image</a>
+                <NavLink to='/' className='go-back'>Back to Main</NavLink>
             </article>
-            <img className='daily-image' src={photo.url} alt='NASA-photo-of-day'></img>
+            <img className='apod-daily-image' src={photo.url} alt='NASA-photo-of-day'></img>
         </section>
     )
 }
