ffdca1c828a143f5ca426c3c53291931ddc7aaf1
Megan Crotteau <megan.crotteau@gmail.com>
2024-04-23 12:24:23 -0600

    Removed unused code from APOD

--- src/APOD/Apod.js
+++ src/APOD/Apod.js
@@ -5,19 +5,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Apod({ photo }) {
-    if (photo.media_type === "video") {
 
-    }
     return (
         <section className="apod" style={{ backgroundImage: `url(${asteroidImage})` }}>
-            <article className="apod-photo-info">
-                <h2>Astronomy Picture of the Day</h2>
-                <h3 className="apod-photo-title">{photo.title}</h3>
-                <p className="apod-photo-cite">{photo.copyright}</p>
-                <p className="apod-photo-explanation">{photo.explanation}</p>
-                <a className="view-fullsize-apod" href={photo.url}>View Fullsize Image</a>
-                <NavLink to="/" className="go-back">Back to Main</NavLink>
-            </article>
             {photo.media_type === "video" ?
                 <div className="apod-daily-image">
                     <iframe
@@ -31,6 +21,14 @@ function Apod({ photo }) {
                 </div> :
                 <img className="apod-daily-image" src={photo.url} alt="NASA-photo-of-day"></img>
             }
+            <article className="apod-photo-info">
+                <h2>Astronomy Picture of the Day</h2>
+                <h3 className="apod-photo-title">{photo.title}</h3>
+                <p className="apod-photo-cite">{photo.copyright}</p>
+                <p className="apod-photo-explanation">{photo.explanation}</p>
+                <a className="view-fullsize-apod" href={photo.url}>View Fullsize Image</a>
+                <NavLink to="/" className="go-back">Back to Main</NavLink>
+            </article>
         </section>
     )
 }
